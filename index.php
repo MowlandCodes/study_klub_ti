3775b3d33e3b8c06741cf4059138ee9df682337b
mowlandcodes <mowlandgaming@gmail.com>
2025-02-25 20:24:17 +0700

    add: added tailwindcss 4.0 modules to project.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Study Klub | UNIDA Gontor</title>
+        <link href="assets/css/app.css" rel="stylesheet">
+        <script>
+          document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+          ':35729/livereload.js?snipver=1"></' + 'script>')
+        </script> <!-- For LiveReload -->
+    </head>
+    <body>
+        <h1 class="text-3xl">Test World</h1>
+    </body>
+</html>
