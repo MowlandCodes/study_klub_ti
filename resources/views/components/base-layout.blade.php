69379368612299aafb1f00826a3cbd2e51091226
mowlandcodes <mowlandgaming@gmail.com>
2025-03-14 20:29:08 +0700

    [faridh] add: added couple of files (home & about template), new component for base-layout and navbar. delete: removed welcome page. edit: added route to / and /about

---
+++ resources/views/components/base-layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Study Klub | TI UNIDA Gontor</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <x-nav-bar></x-nav-bar>
+</body>
+
+</html>
